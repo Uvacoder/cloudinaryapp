6c9f51541de5821d1abc8ad1c417e0e2777c3a1f
Katsote <Katsukeinz@gmail.com>
2021-05-02 18:40:25 -0400

    Añadida la posibilidad de subir 3 imágenes y ajá

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div style="max-height: 300px; max-width: 300px">
       <img style="max-width: 100%; max-height: 100%" :src="imgPreview" alt="">
     </div>
-    <form @submit.prevent="saveImage()" method="post">
+    <form @submit.prevent="saveMultipleImages()" method="post">
       <input type="file" @change="processFile($event)" />
       <button type="submit">subir imagen</button>
     </form>
@@ -18,11 +18,8 @@
         <cld-context cloudName="katsudev">
           <cld-image 
             v-for="img in uploaded"
-            :publicId="img.publicID"
-            :key="img.publicID"
-            width="300"
-            height="200"
-            crop="fit"
+            :publicId="img.publicId"
+            :key="img.publicId"
           />
         </cld-context>
       </div>
@@ -35,9 +32,11 @@
         <span style="font-size: 1rem; font-style: italic">
           Mostradas desde img con la url en el src
         </span>
-        <img 
+        <img
+          v-for="img in uploaded"
           style="width: 50%"
-          src="https://res.cloudinary.com/katsudev/image/upload/v1619820530/Stardew_Valley_4_4_2021_6_46_56_p._m._j2ccn3.png" 
+          :src="img.url" 
+          :key="img.url"
         />
       </h1>
     </div>
@@ -53,6 +52,7 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import skaler from 'skaler';
 
 export default {
   name: 'Home',
@@ -66,45 +66,92 @@ export default {
       CLOUDINARY_API_KEY: "832771279844432",
       CLOUDINARY_API_SECRET: "E90jXPytaCVOFNYGocR5BodX3XA",
       imagen: [],
-      imgPreview: 'https://freepikpsd.com/wp-content/uploads/2019/10/no-image-available-icon-png-8-Transparent-Images.png',
+      imgPreview: '',
       uploaded:
       [
         // {
-          // publicID: '',
+          // publicId: '',
           // url: ''
         // },
       ] 
     }
   },
   methods: {
-     processFile(event) {
+    // Showing Preview and getting image
+    processFile(event) {
       this.imagen = event.target.files[0];
 
       let reader = new FileReader();
+
       reader.onload = (event) => {
         this.imgPreview = event.target.result;
       }
+
       reader.readAsDataURL(this.imagen);
     },
-    saveImage(){
-      // Creatie signature
-      // Doc: https://cloudinary.com/documentation/upload_images#generating_authentication_signatures
-      
+    async editImg(file, opts){
+      return new Promise(async resolve => {
+        const img = await skaler(file, opts);
+        resolve({img});
+      });
+    },
+    async saveImage(){
       // Creating payload
       let formData = new FormData();
       formData.append("file", this.imagen);
       formData.append("api_key", this.CLOUDINARY_API_KEY);
       formData.append("upload_preset", this.CLOUDINARY_UPLOAD_PRESET);
-      formData.append("timestamp", timestamp);
 
-      // Subiendo imagen
-      fetch(this.CLOUDINARY_URL, { method: "POST", body: formData })
-        .then(response => response.json()) //convertimos la respuesta en json
-        .then(data => {
-          this.uploaded.push({publicID: data.public_id, url: data.url})
-          console.log({data});
-        })// obtenemos la url de la imagen guardada
-        .catch(error => console.log("ocurrio un error " , error)); //capturamos un posible error
+      // Upload image
+      return new Promise(async (resolve, reject) => {
+        try{
+          await fetch(this.CLOUDINARY_URL, { method: "POST", body: formData })
+                .then(response => response.json()) //convertimos la respuesta en json
+                .then(data => {
+                  resolve({publicId: data.public_id, url: data.url})
+                })
+                .catch(error => reject({error}));
+        } catch(error){
+          reject(error)
+        }
+
+      });
+    },
+    async saveMultipleImages(){
+      const images = await Promise.all([
+        this.editImg(this.imagen, {width: 200}),
+        this.editImg(this.imagen, {width: 500}),
+        this.editImg(this.imagen, {width: 900})
+      ]);
+
+      let imagesURL = [];
+
+      for (const {img} of images){
+        let saved = false;
+        this.imagen = img;
+
+        await this.saveImage().then(res=>{
+          saved = true;
+          imagesURL.push(res);
+        }).catch(_=>{
+          saved = false;
+          imagesURL = [];
+        });
+
+        if(!saved){
+          alert('Ocurrió un error. Vuelve a intentar');
+          return 0;
+        }
+      }
+
+      // Aqui se pueden subir los enlaces a la BD,
+      // En este caso, guardaré los enlaces en la variable
+      // que uso para mostrar las imagenes subidas
+      if(imagesURL.length){
+        this.uploaded = this.uploaded.concat(imagesURL);
+        imagesURL = [];
+        console.log({uploaded: this.uploaded, imagesURL});
+      }
     }
   },
 }
